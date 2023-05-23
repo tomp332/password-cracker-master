76be19a82ed86288507cb5c9c376b714b7c47b4a
TomP <tomp12@protonmail.com>
2023-05-23 15:54:28 +0300

    feat: Created minion

--- password_cracker_master/__main__.py
+++
@@ -1,7 +0,0 @@
-import uvicorn
-
-from password_cracker_master import master_context
-
-if __name__ == "__main__":
-    uvicorn.run("password_cracker_master.src.server:main_api_router", host=master_context.main_settings.framework_hostname,
-                port=master_context.main_settings.framework_port)
